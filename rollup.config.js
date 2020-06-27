c8077c977b31e0eaf0bfbf05f19165d5e65296e0
Shawn Van Ittersum <svicalifornia@gmail.com>
2020-06-26 21:59:12 -0500

    Update rollup plugins and other dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import { resolve } from 'path';
-import commonjsPlugin from 'rollup-plugin-commonjs';
+import ttypescript from 'ttypescript';
+
+import commonjsPlugin from '@rollup/plugin-commonjs';
+import resolvePlugin from '@rollup/plugin-node-resolve';
+
 import includePathsPlugin from 'rollup-plugin-includepaths';
-// import multiInputPlugin from 'rollup-plugin-multi-input';
 import peerDepsExternalPlugin from 'rollup-plugin-peer-deps-external';
-import resolvePlugin from 'rollup-plugin-node-resolve';
 import typescriptPlugin from 'rollup-plugin-typescript2';
 
 export default {
@@ -25,10 +27,10 @@ export default {
     peerDepsExternalPlugin({
       packageJsonPath: resolve(__dirname, 'package.json'),
     }),
-    // multiInput(),
     resolvePlugin(),
     typescriptPlugin({
       rollupCommonJSResolveHack: true,
+      typescript: ttypescript,
       clean: true,
     }),
     commonjsPlugin(),
