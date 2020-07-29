44dd53f48ac2706ff91e47fc7a1d69505b340558
Shawn Van Ittersum <svicalifornia@gmail.com>
2020-07-29 13:01:26 -0500

    Make update options optional; retain id after saving new Model object

--- src/Model.ts
+++ src/Model.ts
@@ -38,7 +38,7 @@ export default abstract class Model<T extends Model<T>> {
     return true;
   }
 
-  @action async update(values: Partial<T> = {}, options: UpdateOptions<T>) {
+  @action async update(values: Partial<T> = {}, options: UpdateOptions<T> = {}) {
     const repository = options.repository || this._orm.repository;
     if (!repository) {
       throw new Error('Model `update` method called without repository');
@@ -60,10 +60,12 @@ export default abstract class Model<T extends Model<T>> {
     if (!repository) {
       throw new Error('Model `save` method called without repository');
     }
-    const itemId = this[repository.idKey as keyof this] as unknown as Id;
+    const idKey = repository.idKey as keyof this;
+    const itemId = this[idKey] as unknown as Id;
     this._orm.savingPromise = repository[itemId ? 'update' : 'add'](this as unknown as T);
     this._orm.isSaving = true;
     const result = await this._orm.savingPromise;
+    if (result && !itemId) this[idKey] = result[repository.idKey] as any;
     this._orm.isSaving = false;
     return result;
   }
